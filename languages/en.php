1db3cdb5aed04448972bd0f3c18f11d2a50e85dd
fdiebel <fdiebel@emrl.com>
2015-08-24 14:51:20 -0700

    Added EN, more to come

---
+++ languages/en.php
@@ -0,0 +1,49 @@
+<?php
+# American English
+# Language File for Approval
+# https://github.com/EMRL/approval
+# -------
+# Note: when translating to a new language, preserve the original case if possible.
+
+# Approval status
+$lang['approved']='Approved';
+$lang['not-approved']='Not Approved';
+$lang['not-approved-minor']='Not Approved - Minor Changes';
+$lang['not-approved-major']='Not Approved - Major Changes';
+$lang['revert']='Revert to Not Approved';
+
+# Main approval panel
+$lang['title']='Client Approval';
+$lang['overview']='Please carefully check your proof for spelling, puncuation, grammar, names, dates, phone numbers, & email/website addresses.';
+$lang['change-none']='No Changes';
+$lang['change-none-desc']='Proceed with job completion.';
+$lang['change-minor']='Minor Changes';
+$lang['change-minor-desc']='List your changes below; you will be contacted when your updated proof is ready.';
+$lang['change-major']='Major Changes';
+$lang['change-major-desc']='We will call for further discussion.';
+
+# History
+$lang['history']='Approval History';
+$lang['status']='Status';
+$lang['name']='Name';
+$lang['initials']='Initials';
+$lang['approved-by']='Approved by';
+$lang['show-all-history']='Show All History';
+
+# Approval form elements
+$lang['reviewed-by']='Reviewed by';
+$lang['your-name']='Your Name';
+$lang['comments']='Comments & Corrections (if applicable)';
+$lang['initials-desc']='Please enter your initials below and click the Submit button.';
+$lang['fine-print']='By initialing this form you are acknowledging receipt of this document, and verifying that you have carefully reviewed the artwork contained herein.';
+$lang['reviewed-on']='Reviewed on';
+$lang['submit']='Submit';
+
+# Settings page
+$lang['email-users']='Email to Users';
+$lang['email-users-desc']='These are the internal users that will receive approval emails.';
+$lang['email-subject']='Email Subject';
+$lang['email-message']='Default Email Message';
+$lang['email-message-desc']='Used as the default email message when sending an asset to clients.';
+?>
+
